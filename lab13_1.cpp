f2f6d19e854287aa795b5bd82f9e7652c6ea0220
= <=>
2023-01-10 15:03:49 +0700

    Complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    {
+    if (x == 0)
+    {
+        x = 0;
+    }else if (x == 1)
+    {
+        x = 1;
+    }
+    else if(x > 1)
+    {
+    x = fibonacci(x-1) + fibonacci(x-2);
+    }
+    }
+    return x;
 }
