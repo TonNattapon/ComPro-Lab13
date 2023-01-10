b5adc29b7278912e6256277f07a79c39d5c27b1d
Nattapon Chanaveroj <38356005+TonNattapon@users.noreply.github.com>
2023-01-10 16:05:37 +0700

    Update lab13_3.cpp

--- lab13_3.cpp
+++ lab13_3.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void mySwap(int &,int &); 
 
+
 int main(){
 	int x,y;
 	cin >> x >> y;
@@ -14,4 +15,11 @@ int main(){
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
+}
+void mySwap(int &x,int &y)
+{
+	int z;
+	z=x;
+	x=y;
+	y=z;
 }
